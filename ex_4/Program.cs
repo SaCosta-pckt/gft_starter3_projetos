54a2009c756ac4ab921c5e2d6e081443cb237875
SaCosta_pckt <sabrina.costa06@outlook.com>
2022-02-08 17:43:20 -0300

    projetos sendo finalizados

--- ex_4/Program.cs
+++ ex_4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Inverter um inteiro
+using static System.Console;
+namespace ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long [] nums = {15485448, 2195498, 12, 98716549, 9999998999, 1654891};
+            foreach (var item in nums)
+            {
+                string nIntertido = new string (item.ToString().Reverse().ToArray());
+                WriteLine($"{item} {nIntertido}");
+                
+            }
+        }
+    }
+}
