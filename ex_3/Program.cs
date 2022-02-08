1d491fb44cb8b6bdba480fd050f0cb603361c151
SaCosta_pckt <sabrina.costa06@outlook.com>
2022-02-08 17:27:13 -0300

    continuando os projetos

---
+++ ex_3/Program.cs
@@ -0,0 +1,38 @@
+﻿//Letra que não se repete
+using static System.Console;
+namespace ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] testes = {"aaabccccdeeef", "strings", "microsoft visual studio 2022", "abcedoftz"};
+            foreach (var item in testes)
+            {
+                WriteLine(NaoRepete(item));
+            }
+        }
+        public static string NaoRepete(string s){
+            int cont=0;
+            string result="_";
+            foreach (char i in s)
+            {
+                cont = 0;
+                foreach (char j in s)
+                {
+                    if (j==i){
+                        cont ++;
+                    }
+                }
+                
+                if (cont<=1){
+                    result = i.ToString();
+                    break;
+                }
+            }
+
+            
+            return result;
+        }
+    }
+}
