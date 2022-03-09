c76b5e5e2400cd0a1991635d1372d6591823130e
SaCosta_pckt <sabrina.costa06@outlook.com>
2022-03-09 14:28:10 -0300

    implementando melhorias

--- ex_1/Program.cs
+++ ex_1/Program.cs
@@ -1,10 +1,22 @@
 ﻿using static System.Console;
+using ex_1.src;
+
 namespace ex_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Veiculo v1 = new Veiculo();
+            v1.Desligar();
+            v1.Ligar();
+            v1.Pintar("vermelho");
+            v1.Abastecer(60);
+            v1.Acelerar();
+            v1.Ligar();
+            v1.Frear();
+            v1.Desligar();
+            v1.Abastecer(1);
             
         }
     }
--- ex_1/src/Veiculo.cs
+++ ex_1/src/Veiculo.cs
@@ -1,3 +1,4 @@
+using static System.Console;
 namespace ex_1.src
 {
     public class Veiculo
@@ -6,7 +7,7 @@ namespace ex_1.src
         private string modelo ="";
         private string placa ="";
         private string cor ="";
-        private string km ="";
+        private float km;
         private bool isLigado;
         private int litrosCombustivel;
         private int velocidade;
@@ -28,7 +29,7 @@ namespace ex_1.src
         public string getPlaca(){
             return this.placa;
         }
-        public string getKm(){
+        public float getKm(){
             return this.km;
         }
         public bool getIsLigado(){
@@ -57,7 +58,7 @@ namespace ex_1.src
         public void setCor(string cor){
             this.cor=cor;
         }
-        public void setKm(string km){
+        public void setKm(float km){
             this.km=km;
         }
         public void setIsLigado(bool isLigado){
@@ -75,17 +76,61 @@ namespace ex_1.src
 
         //Métodos
         public void Acelerar(){
-            this.velocidade+=20;
+            if (getIsLigado() == true && getLitrosCombustivel() > 0){
+                this.velocidade+=20;
+                WriteLine("Você acelerou. Velocidade = " + getVelocidade());
+            }
+            else {
+                throw new System.Exception(message: "Não é possível acelerar com o carro desligado ou sem gasolina!");
+            }
         }
 
         public void Abastecer(int litros){
-            if(litrosCombustivel+litros > 60){
-                this.litrosCombustivel+=litros;
-            } 
+            // adiocionar exception do limite 
+            if(getLitrosCombustivel()+litros <= 60){
+                setLitrosCombustivel(getLitrosCombustivel()+litros);
+                WriteLine("Você abasteceu. Combustível atual = " + getLitrosCombustivel());
+            }
+            else {
+                throw new System.Exception(message: $"Não é possível abastecer. O tanque transbordará. Combustível atual = {getLitrosCombustivel()}");
+            }
         }
 
         public void Frear(){
-            this.velocidade-=20;
+            if (velocidade!=0){
+                setVelocidade(getVelocidade() - 20);
+                WriteLine("Você freiou. Velocidade atual = " + getVelocidade());
+            }
+            else {
+                WriteLine("Não é possível freiar. O veículo já está parado.");
+            }
+        }
+
+        public void Pintar(string cor){
+            setCor(cor);
+            WriteLine("O veículo foi pintado de " + getCor());
+        }
+
+        public void Ligar(){
+            if (isLigado == false){
+                setIsLigado(true);
+                WriteLine("O veículo ligou");
+            }
+            else {
+                WriteLine("O veículo já está ligado.");
+            }
+        }
+
+        public void Desligar(){
+            if (velocidade == 0){
+                if (isLigado == true){
+                    setIsLigado(false);
+                    WriteLine("O veículo desligou");
+                }
+            }
+            else {
+                WriteLine("Não é possível desligar. O veículo já está desligado ou em movimento.");
+            }
         }
     }
 }
